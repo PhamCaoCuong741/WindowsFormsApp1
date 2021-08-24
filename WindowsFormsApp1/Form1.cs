f6818ed109650b61736006b9f20cf68b7d1a7f91
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-08-24 21:16:04 +0700

    Phạm Cương

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,5 +17,41 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        int tong = 0, tongchan = 0, tongle = 0;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            txtDaySo.Text = txtDaySo.Text + txtNhap.Text + ",";
+
+            int iNhap = Convert.ToInt32(txtNhap.Text);
+            tong = tong + iNhap;
+            txtTong.Text = tong.ToString();
+            if (iNhap % 2 == 0)
+            {
+
+                tongchan = tongchan + iNhap;
+
+            }
+            else
+            {
+                tongle = tongle + iNhap;
+            }
+            txtTongChan.Text = tongchan.ToString();
+            txtTongle.Text = tongle.ToString();
+
+        }
     }
 }
